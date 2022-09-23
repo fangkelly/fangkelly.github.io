f530c4c0a7e12d77eeac30208ee88b4025607f06
Kelly Fang <kellyfangg@gmail.com>
2022-09-23 04:31:48 -0400

    added intro sentence

---
+++ index.js
@@ -0,0 +1,14 @@
+function overlay(div) {
+    console.log(`overlay ${div}`)
+    document.getElementById(`${div}-overlay`).classList.remove("hide")
+    document.getElementById(`${div}-overlay`).classList.add("show")
+}
+
+
+function unoverlay(div) {
+    console.log(`unoverlay ${div}`)
+    document.getElementById(`${div}-overlay`).classList.remove("show")
+    document.getElementById(`${div}-overlay`).classList.add("hide")
+
+}
+
