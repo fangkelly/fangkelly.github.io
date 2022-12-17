f47d9ad65243b69552d99b48e3982b9daf9806e6
Kelly Fang <kellyfangg@gmail.com>
2022-12-16 21:54:08 -0500

    x

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 function overlay(div) {
-    console.log(`overlay ${div}`)
     document.getElementById(`${div}-overlay`).classList.remove("hide")
     document.getElementById(`${div}-overlay`).classList.add("show")
 }
 
 
 function unoverlay(div) {
-    console.log(`unoverlay ${div}`)
     document.getElementById(`${div}-overlay`).classList.remove("show")
     document.getElementById(`${div}-overlay`).classList.add("hide")
 
 }
 
 function clickLink(link) {
-    console.log("clicked")
     window.open(link, '_blank');
 
 }
